5b66b1313a84c27f57670910112cea2e2f7ba223
davi38 <davi4alves@gmail.com>
2022-10-15 02:58:39 -0300

    feat: added production env url

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home, ChatPage } from '../components/'
 import { io } from 'socket.io-client'
-const socket = io('http://localhost:3001')
+const socket = io(import.meta.env.VITE_PROD_URL)
+
 
 const App = () => {
   return (
