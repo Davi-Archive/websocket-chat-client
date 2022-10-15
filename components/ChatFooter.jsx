10c70705d76359bb0d94a0294b4e632bc56475d9
davi38 <davi4alves@gmail.com>
2022-10-15 00:18:20 -0300

    feat: socket connection

--- components/ChatFooter.jsx
+++ components/ChatFooter.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 
 const ChatFooter = () => {
-    return (
-        <div className="chat__sidebar">
-            <h2>Open Chat</h2>
+    const [message, setMessage] = useState('')
 
-            <div>
-                <h4 className="chat__header">ACTIVE USERS</h4>
-                <div className="chat__users">
-                    <p>User 1</p>
-                    <p>User 2</p>
-                    <p>User 3</p>
-                    <p>User 4</p>
-                </div>
-            </div>
+    const handleSendMessage = (e) => {
+        e.preventDefault()
+        console.log({ username: localStorage.getItem('username'), message })
+        setMessage('')
+    }
+    return (
+        <div className="chat__footer">
+            <form className="form" onSubmit={handleSendMessage}>
+                <input
+                    type="text"
+                    placeholder="Write message"
+                    className="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button className="sendBtn">SEND</button>
+            </form>
         </div>
     )
 }
