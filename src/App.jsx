73c00fa279ba1aa8b7d67f67542b69340b81ac1e
davi38 <davi4alves@gmail.com>
2022-10-14 23:48:50 -0300

    feat:Home component and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { io } from 'socket.io-client'
+const socket = io('http://localhost:3001')
 
 function App() {
   return (
-    <div className="App">
-     <h1>App</h1>
-    </div>
+    <>
+      {console.log(socket)}
+      < div className="App" >
+        <h1>App</h1>
+      </div >
+    </>
   )
 }
 
